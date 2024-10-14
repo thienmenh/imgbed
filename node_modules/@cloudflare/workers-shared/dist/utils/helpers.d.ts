bf4c8d38d387df1c17fd7c9c608180824bb453bb
thienmenh <github.dismiss092@passinbox.com>
2024-10-14 16:11:46 +0700

    update file

---
+++ node_modules/@cloudflare/workers-shared/dist/utils/helpers.d.ts
@@ -0,0 +1,3 @@
+/** normalises sep for windows and prefix with `/` */
+export declare const normalizeFilePath: (relativeFilepath: string) => string;
+export declare const getContentType: (absFilePath: string) => string;
